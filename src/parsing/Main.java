bb61ad787a76431665603b6192903b2b0d8941fd
Maks Burenko <Cherniymaks@yandex.ru>
2022-01-31 14:28:26 +0300

    123

--- src/parsing/Main.java
+++ src/parsing/Main.java
@@ -6,18 +6,18 @@ public class Main {
                 + "by invoking each other's methods. Every object is of a certain type,"
                 + "and that type is defined by a class or an interface. Most Java programs"
                 + "use a collection of objects of many different types.";
-        String [] array = s.split("\\. ");
+        String [] array = s.split(" ");
+ //       for (String temp : array) {
+ //              System.out.println(temp);
+ //       }
         for (String temp : array) {
-            if (temp.endsWith(".")) {
-                System.out.println(temp);
-            } else {
-                System.out.println(temp + ".");
+            Pattern p = Pattern.compile(" .+?ing");
+            Matcher m = p.matcher (temp);
+            while (m.find()){
+                System.out.println(m.start() + " " + m.group());
             }
-        }
-        Pattern p = Pattern.compile("^A | A | a ");
-        Matcher m = p.matcher (s);
-        while (m.find()){
-            System.out.println(m.start() + " " + m.group());
         }
     }
 }
+
+// https://docs.oracle.com/javase/8/docs/api/
